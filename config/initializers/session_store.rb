3f5a4236307cc22d049cfe3b42d2a03904fcbb7a
Winnie Ngo <ngowinn@gmail.com>
2016-09-28 23:03:02 -0700

    add gitignore

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_crib_mapper_session'
+Rails.application.config.session_store :cookie_store, key: '_cribMapper_session'
